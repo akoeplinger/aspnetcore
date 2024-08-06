8c7b97573484d86882d498787bf9080c0ec72ace
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-08-06 15:03:54 +0000

    Assert.Empty

--- src/Mvc/Mvc.Core/test/CreatedResultTests.cs
+++ src/Mvc/Mvc.Core/test/CreatedResultTests.cs
@@ -88,7 +88,7 @@ public class CreatedResultTests
 
         // Assert
         Assert.Equal(StatusCodes.Status201Created, httpContext.Response.StatusCode);
-        AssertEqual(0, httpContext.Response.Headers["Location"].Count);
+        Assert.Equal(0, httpContext.Response.Headers["Location"].Count);
     }
 
     [Fact]
